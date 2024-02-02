2d8a23e6d45620556b9cbb63f112311b22b5d5d3
Aidan Hamwood <ajh@tuta.io>
2024-02-03 07:29:43 +1000

    Visual design first iteration

--- api/entrypoint.js
+++ api/entrypoint.js
@@ -5,13 +5,17 @@ export default (req, res) => {
   try {
     const url = new URL(req.url, `https://${req.headers.host}`),
           filepath = path.join(process.cwd(), "public", url.pathname === "/" ? "index.html" : url.pathname);
+    let enc;
     switch (path.extname(filepath)) {
-      case ".html": res.setHeader('Content-Type', 'text/html'); break;
-      case ".js": res.setHeader('Content-Type', 'application/javascript'); break;
+      case ".html": res.setHeader('Content-Type', 'text/html'); enc = 'utf8'; break;
+      case ".js": res.setHeader('Content-Type', 'application/javascript'); enc = 'utf8'; break;
+      case ".woff": res.setHeader('Content-Type', 'font/woff'); break;
+      case ".woff2": res.setHeader('Content-Type', 'font/woff2'); break;
+      case ".svg": res.setHeader('Content-Type', 'image/svg+xml'); enc = 'utf8'; break;
       default: return notfound(res)
     }
     if (!existsSync(filepath)) return notfound(res);
-    const body = readFileSync(filepath, 'utf8');
+    const body = readFileSync(filepath, enc);
     return res.status(200).end(body)
   } catch (e) { return servererror(res) }
 }
